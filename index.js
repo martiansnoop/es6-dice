eca1c9e55d563a8dd610043887c4f46c2b77d9e2
Emily Wenberg <martiansnoop@gmail.com>
2015-02-22 17:06:14 -0800

    Add a roll class to encapsulate an instance of rolling

--- index.js
+++ index.js
@@ -6,10 +6,20 @@ export function run() {
     var naturalOne = new LoadedDie(20, 1);
 
     var results = roll(d20, naturalOne);
-    console.log(results);
+    console.log(results.map(r => r.toString()));
 }
 
 function roll(...dice) {
-    return dice.map(d => d.roll());
+    return dice.map(d => new Roll(d.roll(), d));
+}
+
+class Roll {
+    constructor(result, die) {
+        this.result = result;
+        this.die = die;
+    }
+    toString() {
+        return this.die.toString() + ": " + this.result;
+    }
 }
 
